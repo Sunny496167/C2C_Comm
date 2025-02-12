66a6981e299e79ff2b4539a88c7e1c7db6115fb0
Sunny Kumar <sunnykumar49617@gmail.com>
2025-02-12 07:29:37 +0530

    add button component

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,11 +1,21 @@
+import Button from "./Components/Button";
 
 
 const App = () => {
   return (
-    <div>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    <div className="flex gap-4 p-10">
+      <Button 
+        text="Green to Blue" 
+        gradient="from-green-400 to-blue-500" 
+        focusGradient="ring-green-100" 
+      />
+      <Button 
+        text="Pink to Red" 
+        gradient="from-pink-500 to-red-500" 
+        focusGradient="ring-pink-100" 
+      />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
