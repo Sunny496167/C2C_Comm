4c08b6c27ea71f0716f343ec87502c2479dcc627
Sunny Kumar <sunnykumar49617@gmail.com>
2025-02-16 22:50:38 +0530

    add authetication feature

--- Backend/index.js
+++ Backend/index.js
@@ -1,20 +1,35 @@
-require("dotenv").config();
-const express = require("express");
-const cors = require("cors");
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import path from "path";
+
+import { connectDB } from "./db/connectDB.js";
+
+import authRoutes from "./routes/auth.route.js";
+
+dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8000;
+const __dirname = path.resolve();
 
-// Middleware
-app.use(express.json());
-app.use(cors());
+app.use(cors({ origin: "http://localhost:5173", credentials: true }));
 
-// Sample Route
-app.get("/", (req, res) => {
-  res.send("Backend is running...");
-});
+app.use(express.json()); // allows us to parse incoming requests:req.body
+app.use(cookieParser()); // allows us to parse incoming cookies
 
-// Start Server
+app.use("/api/auth", authRoutes);
+
+if (process.env.NODE_ENV === "production") {
+	app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+	app.get("*", (req, res) => {
+		res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+	});
+}
+
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+	connectDB();
+	console.log("Server is running on port: ", PORT);
 });
