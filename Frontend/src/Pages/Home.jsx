f72d39a7d615b509d2f3292840b6fa59530751b4
Sunny Kumar <sunnykumar49617@gmail.com>
2025-09-13 23:48:07 +0530

    git reset

--- Frontend/src/Pages/Home.jsx
+++
@@ -1,9 +0,0 @@
-import Button from "../Components/Button"
-
-const Home = () => {
-  return (
-    <div><Button type="normal" text="Outline Button" gradient="from-pink-500 to-red-500" /></div>
-  )
-}
-
-export default Home
